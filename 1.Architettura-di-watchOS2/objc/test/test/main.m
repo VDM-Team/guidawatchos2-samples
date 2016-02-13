783a0eee89cd00e7d532b850d0bb53c625546a1a
Francesco Ingrassia <fra@---.homenet.telecomitalia.it>
2016-02-13 12:09:15 +0100

    aggiunto capitolo 1.Architettura-di-watchOS2

---
+++ 1.Architettura-di-watchOS2/objc/test/test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  test
+//
+//  Created by Francesco Ingrassia on 13/02/16.
+//  Copyright © 2016 Francesco Ingrassia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
