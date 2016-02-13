d44b379a4b0dbe8e57d0d9c19a1683c7f8c631b9
Francesco Ingrassia <fra@MacBook-Pro-di-Francesco.local>
2016-02-13 11:29:14 +0100

    creata struttura dei progetti divisi per capitoli

---
+++ 2.Panoramica-dei-Componenti/objc/test/test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by Francesco Ingrassia on 13/02/16.
+//  Copyright © 2016 Francesco Ingrassia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 3.Glance-e-Notifiche/objc/test/test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by Francesco Ingrassia on 13/02/16.
+//  Copyright © 2016 Francesco Ingrassia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
